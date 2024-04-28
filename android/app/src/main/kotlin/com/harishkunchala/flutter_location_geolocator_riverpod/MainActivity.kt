a9c40084f568f8c010962ea546d1fdc32e440536
Harish Kunchala <32921476+khkred@users.noreply.github.com>
2024-04-27 18:58:21 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/harishkunchala/flutter_location_geolocator_riverpod/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.harishkunchala.flutter_location_geolocator_riverpod
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/features/location/data/location_permission_enum.dart
@@ -0,0 +1,16 @@
+enum LocationPermission {
+  /// The location permission is denied.
+  denied,
+
+  /// The location permission is granted.
+  granted,
+
+  /// The location permission is denied forever.
+  deniedForever,
+
+  /// The location permission is unknown.
+  unknown,
+
+  /// While in Use
+  whileInUse,
+}
